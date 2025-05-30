3a0149382cd9d4bf8f8bbdbfacc091725b27e983
mrosa16 <michaelpr3003@gmail.com>
2025-05-30 15:35:08 -0300

    feat: documentation into projetct

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,15 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty()
   @IsNotEmpty()
   nome!: string;
-
+  @ApiProperty()
   @IsNotEmpty()
   sobrenome!: string;
-
+  @ApiProperty()
   @IsEmail()
   email!: string;
-
+  @ApiProperty()
   @IsNotEmpty()
   senha!: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,16 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Alume API')
+    .setDescription('Documentação da API da aplicação Alume')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
